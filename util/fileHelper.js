dc9cc35f11f7c06aa874f04710af1c9c6946a7bd
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2023-12-19 20:45:58 +0530

    fix: store data securely

--- util/fileHelper.js
+++ util/fileHelper.js
@@ -1,8 +1,30 @@
 import fs from 'fs/promises';
-const filePath = 'data.json';
+import path from 'path';
+import os from 'os';
 
-async function  writeToPath(data) {
+const fileName = '.data.json';
+const homeDirectory = os.homedir();
+const dataDirectory = path.join(homeDirectory, 'bhaiya');
+const filePath = path.join(dataDirectory, fileName);
+
+async function ensureDirectoryExists(directory) {
   try {
+    await fs.access(directory);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      // Directory doesn't exist, create it
+      await fs.mkdir(directory, { recursive: true });
+    } else {
+      throw error;
+    }
+  }
+}
+
+async function writeToPath(data) {
+  try {
+    // Ensure the directory exists
+    await ensureDirectoryExists(dataDirectory);
+
     const jsonData = JSON.stringify(data, null, 2);
     await fs.writeFile(filePath, jsonData, 'utf-8');
   } catch (error) {
@@ -17,6 +39,7 @@ async function readFromPath() {
   } catch (error) {
     if (error.code === 'ENOENT') {
       // File doesn't exist, create it with an empty array
+      await ensureDirectoryExists(dataDirectory);
       await fs.writeFile(filePath, '[]', 'utf-8');
       return [];
     }
